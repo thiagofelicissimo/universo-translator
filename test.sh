5dd49b32960365ffb1a90d2980f33cda45a7cca1
Ashish Barnawal <ashbrnwl.2000@gmail.com>
2022-01-16 00:02:53 +0530

    better test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 set -e
 
+usage () {
+echo \
+"$0 [OPTION]... [FILE]...
+
+-t	Compile the theory/*.dk files
+-e	Encode sttfa file to cts in ctslib/
+-l	Compile cts Lib files in ctslib/
+-u	run Universo on FILES
+-m	Merge solution in final/
+-a	run All of the above
+-v	Verbose mode
+-h	show Help
+"
+}
+
 generate_graph () {
-	echo "digraph {" > graph.dot
-	(cd lib && dkdep -i $*) | \
+	local dotfile=$(mktemp)
+
+	echo "digraph {" > $dotfile
+	(cd lib && dkdep -i ${files[@]}) | \
 	sed 	-e 's/:/-> {/' \
 		-e 's/lib\///g' \
 		-e 's/$/ };|/' \
@@ -11,86 +28,145 @@ generate_graph () {
 		-e 's/\.dko//g' \
 		-e 's/^/   /' | \
 	tr '|' '\n' \
-	>> graph.dot
-	echo "}" >> graph.dot
+	>> $dotfile
+	echo "}" >> $dotfile
 
-	dot -Tpdf graph.dot -o depend.pdf
+	dot -Tpdf $dotfile -o depend.pdf
+
+	rm -rf $dotfile
+}
+
+lib2ctslib () {
+	mkdir -p ctslib
+	for f in ${files[@]}
+	do
+		# echo "[dkmeta] converting lib/$f"
+
+		OCAMLRUNPARAM='b' \
+		dune exec dkmeta -- -I ./theory \
+		 -m meta/meta.dk lib/$f > ctslib/$f
+	done
+}
+
+check_theory () {
+	dkcheck -eq theory/*.dk
+}
+
+clean () {
+	rm -rf *.dko output/ _build/ ctslib/ final/
+}
+
+# ensure files are in format like eq.dk (not eq, lib/eq.dk)
+clean_files () {
+        echo "$@"       | \
+        tr ' ' '\n'     | \
+        sed -e '/^ *$/d' \
+            -e 's/^lib\///' \
+            -e 's/\.dk$//' \
+            -e 's/$/.dk/' | \
+        tr '\n' ' '
+}
+
+compile_ctslib () {
+	# printf reuses same pattern until args exhaust
+	dkcheck -I ./theory -I ./ctslib/ -e $(printf "ctslib/%s " ${files[@]})
+}
+
+run_universo () {
+	OCAMLRUNPARAM='b' \
+	dune exec universo -- -o output \
+	--theory theory/cts.dk --config config/universo_cfg.dk \
+	-I theory/ \
+	$(printf "ctslib/%s " $files)
+}
+
+merge_solution () {
+	mkdir -p final
+
+	for f in $files
+	do
+		base=$(basename $f .dk)
+
+	        OCAMLRUNPARAM='b' \
+		dune exec dkmeta -- -m output/`echo $base`_sol.dk \
+		output/$base.dk -I theory/ -I output/ \
+		> final/$base.dk
+
+		sed -i 's/#REQUIRE[^.]*.//' final/$base.dk
+		sed -i '/./,$!d' final/$base.dk
+	done
+
+
+	echo "
+
+	CHECKING FINAL FILES
+	====================
+	"
+
+	dkcheck -e -I ./theory/ -I ./final/ $(dkdep -si -I ./final $(printf "./final/%s " $files))
+
 }
 
 
-rm -rf *.dko output/ _build/ ctslib/ final/
-mkdir ctslib
+# main <file1> <file2> ...
+main () {
 
-dkcheck -eq theory/*.dk
+	while getopts 'tecumavh' c ; do
+	case $c in
+		t) theocomp=true ;;
+		e) stffaenc=true ;;
+		g) gengraph=true ;;
+		l) ctscomp=true ;;
+		u) rununiv=true ;;
+		m) mergesol=true ;;
+		a) runall=true ;;
+		v) verbose=true ;;
+		h) usage ; return 0 ;;
+	esac
+	done
 
-# use all files in lib/ if no args are given
-if [ $# -eq 0 ]; then
-	files=$(ls -1 lib/*.dk | tr '\n' ' ')
-else
-	files=$@
-fi
+	shift $((OPTIND - 1))
 
+	echo "@ = $@ ; # = $#"
 
-# files=${@:-$(ls -1 lib/*.dk | tr '\n' ' ')}
+	# use all files in lib/ if no args are given
+	if [ $# -eq 0 ]; then
+		files=$(ls -1 lib/*.dk | tr '\n' ' ')
+	else
+		files=$@
+	fi
 
-# ensure files are in format like eq.dk (not eq, lib/eq.dk)
-files=$(echo "$files" | \
-        tr ' ' '\n' | \
-	sed -e '/^ *$/d' \
-	    -e 's/^lib\///' \
-            -e 's/\.dk$//' \
-	    -e 's/$/.dk/' | \
-	tr '\n' ' ')
+	files=`clean_files $files`
 
+	# ensure files are in order of dependencies
+	files=$(cd lib && dkdep -si $files)
 
-# ensure files are in order of dependencies
-files=$(cd lib && dkdep -si $files) 
+	# echo "files = $files"
 
-# Generate graph
-generate_graph $files
+	if [[ $runall = true ]] || [[ $theocomp = true ]]; then
+		echo "Checking theory files..."
+		check_theory
+	fi
 
+	if [[ $runall = true ]] || [[ $sttfaenc = true ]]; then
+		lib2ctslib
+	fi
 
-for f in ${files[@]}
-do
-	# echo "[dkmeta] converting lib/$f"
+	if [[ $runall = true ]] || [[ $gengraph = true ]]; then
+		generate_graph
+	fi
 
-	OCAMLRUNPARAM='b' \
-	dune exec dkmeta -- -I ./theory \
-	 -m meta/meta.dk lib/$f > ctslib/$f
-done
+	if [[ $runall = true ]] || [[ $ctscomp = true ]]; then
+		compile_ctslib
+	fi
 
+	if [[ $runall = true ]] || [[ $rununiv = true ]]; then
+		run_universo
+	fi
 
-# printf reuses same pattern until args exhaust
-dkcheck -q -I ./theory -I ./ctslib/ -e $(printf "ctslib/%s " ${files[@]}) 
+	if [[ $runall = true ]] || [[ $mergesol = true ]]; then
+		merge_solution
+	fi
+}
 
-
-OCAMLRUNPARAM='b' \
-dune exec universo -- -o output \
---theory theory/cts.dk --config config/universo_cfg.dk \
--I theory/ \
-$(printf "ctslib/%s " $files)
-
-
-mkdir final
-for f in $files
-do
-	base=$(basename $f .dk)
-
-        OCAMLRUNPARAM='b' \
-	dune exec dkmeta -- -m output/`echo $base`_sol.dk \
-	output/$base.dk -I theory/ -I output/ \
-	> final/$base.dk
-
-	sed -i 's/#REQUIRE[^.]*.//' final/$base.dk
-	sed -i '/./,$!d' final/$base.dk
-done
-
-
-echo "
-
-CHECKING FINAL FILES
-====================
-"
-
-dkcheck -e -I ./theory/ -I ./final/ $(dkdep -si -I ./final ./final/*.dk)
-
+main $@
