6f8bbf336dc619674a056a6b190fdee196337488
Ashish Barnawal <ashbrnwl.2000@gmail.com>
2022-01-15 18:42:50 +0530

    use splitted library

--- test.sh
+++ test.sh
@@ -1,36 +1,78 @@
 #!/bin/bash
 set -e
+
+generate_graph () {
+	echo "digraph {" > graph.dot
+	(cd lib && dkdep -i $*) | \
+	sed 	-e 's/:/-> {/' \
+		-e 's/lib\///g' \
+		-e 's/$/ };|/' \
+		-e 's/\w*\.dk //' \
+		-e 's/\.dko//g' \
+		-e 's/^/   /' | \
+	tr '|' '\n' \
+	>> graph.dot
+	echo "}" >> graph.dot
+
+	dot -Tpdf graph.dot -o depend.pdf
+}
+
+
 rm -rf *.dko output/ _build/ ctslib/ final/
 mkdir ctslib
 
-dkcheck -e theory/*.dk
+dkcheck -eq theory/*.dk
 
-for f in lib/*.dk
+# use all files in lib/ if no args are given
+if [ $# -eq 0 ]; then
+	files=$(ls -1 lib/*.dk | tr '\n' ' ')
+else
+	files=$@
+fi
+
+
+# files=${@:-$(ls -1 lib/*.dk | tr '\n' ' ')}
+
+# ensure files are in format like eq.dk (not eq, lib/eq.dk)
+files=$(echo "$files" | \
+        tr ' ' '\n' | \
+	sed -e '/^ *$/d' \
+	    -e 's/^lib\///' \
+            -e 's/\.dk$//' \
+	    -e 's/$/.dk/' | \
+	tr '\n' ' ')
+
+
+# ensure files are in order of dependencies
+files=$(cd lib && dkdep -si $files) 
+
+# Generate graph
+generate_graph $files
+
+
+for f in ${files[@]}
 do
-	echo "[dkmeta] converting $f"
-	
+	# echo "[dkmeta] converting lib/$f"
+
 	OCAMLRUNPARAM='b' \
 	dune exec dkmeta -- -I ./theory \
-	 -m meta/meta.dk $f > ctslib/$(basename $f)
+	 -m meta/meta.dk lib/$f > ctslib/$f
 done
 
 
-files=$(dkdep -I ./theory/ -I ./ctslib/ -s theory/*.dk ctslib/*.dk)
-lib_files=$(echo $files | sed 's/[^ ]*theory\/[^ ]*.dk//g' -)
+# printf reuses same pattern until args exhaust
+dkcheck -q -I ./theory -I ./ctslib/ -e $(printf "ctslib/%s " ${files[@]}) 
 
-dkcheck -I ./theory -I ./ctslib/ -e $files
 
 OCAMLRUNPARAM='b' \
 dune exec universo -- -o output \
 --theory theory/cts.dk --config config/universo_cfg.dk \
--I theory/ -l \
-$lib_files
-
-
+-I theory/ \
+$(printf "ctslib/%s " $files)
 
 
 mkdir final
-for f in $lib_files
+for f in $files
 do
 	base=$(basename $f .dk)
 
@@ -43,10 +85,12 @@ do
 	sed -i '/./,$!d' final/$base.dk
 done
 
+
 echo "
 
 CHECKING FINAL FILES
 ====================
 "
 
-dkcheck -e -I ./theory/ -I ./final/ $(dkdep -si -I ./final -I ./theory ./final/*.dk)
+dkcheck -e -I ./theory/ -I ./final/ $(dkdep -si -I ./final ./final/*.dk)
+
